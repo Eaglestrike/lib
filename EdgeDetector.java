8914c7b519b28fabbbaf7ddd6b5ee04a86f63f91
Aris (Karim) Merchant <wizkarim@gmail.com>
2017-11-06 15:14:33 -0800

    Import library

---
+++ EdgeDetector.java
@@ -0,0 +1,51 @@
+package org.usfirst.frc.team114.lib;
+
+import java.util.concurrent.Callable;
+
+public class EdgeDetector {
+    public enum EdgeType {
+        FLAT, RISING, FALLING
+    }
+
+    private Callable<Boolean> lambda;
+    private boolean lastValue = false;
+
+    public EdgeDetector(Callable<Boolean> lambda) {
+        this.lambda = lambda;
+    }
+
+    public EdgeType getEdge() {
+        boolean newValue;
+        try {
+            newValue = lambda.call();
+        } catch (Exception e) {
+            newValue = false;
+        }
+
+        boolean lastValue = this.lastValue;
+        this.lastValue = newValue;
+
+        // true -> false
+        if (lastValue && !newValue)
+            return EdgeType.FALLING;
+
+        // false -> true
+        if (!lastValue && newValue)
+            return EdgeType.RISING;
+
+        // (false -> false) | (true -> true)
+        return EdgeType.FLAT;
+    }
+
+    public boolean falling() {
+        return getEdge() == EdgeType.FALLING;
+    }
+
+    public boolean rising() {
+        return getEdge() == EdgeType.RISING;
+    }
+
+    public boolean flatlining() {
+        return getEdge() == EdgeType.FLAT;
+    }
+}
