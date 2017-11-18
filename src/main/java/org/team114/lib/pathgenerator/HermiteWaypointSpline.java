8cd468a8ed23092eff7b583c03bfe997128ce17e
Aris (Karim) Merchant <wizkarim@gmail.com>
2017-11-17 23:27:16 -0800

    More formatting, mostly whitespace

--- src/main/java/org/team114/lib/pathgenerator/HermiteWaypointSpline.java
+++ src/main/java/org/team114/lib/pathgenerator/HermiteWaypointSpline.java
@@ -8,184 +8,195 @@ import org.ejml.simple.SimpleMatrix;
 /**
  * A HermiteWaypointSpline constructs a Spline based on a series of Waypoints
  * using a piecewise function of parametric cubic interpolating spline sections.
+ *
  * @link https://people.cs.clemson.edu/~dhouse/courses/405/notes/splines.pdf
  */
 public class HermiteWaypointSpline {
 
 
-	/** List of points and matrices containing spline coefficients */
-	private final List<SimpleMatrix> splineSections = new ArrayList<>();
-	private final List<Waypoint> pointList = new ArrayList<>();
+    /** List of points and matrices containing spline coefficients. */
+    private final List<SimpleMatrix> splineSections = new ArrayList<>();
+    private final List<Waypoint> pointList = new ArrayList<>();
 
-	/** Generic multiplier for Hermite splines which is needed to find the solution */
-	private static final SimpleMatrix multBase = new SimpleMatrix( new double[][] {{1, 0, 0, 0},
-      {1, 1, 1, 1}, {0, 1, 0, 0}, {0, 1, 2, 3}} ).invert();
-	
-	private boolean safeReturn = false;
-	/**
-     * A HermiteSpline instance will take points and handle finding which function to use to find the
-     * solution points are formatted in.
-	 * @param points Waypoint[] A list of Waypoints to build the spline off of
-	 */
-	public HermiteWaypointSpline(Waypoint[] points) {
-		for (Waypoint p : points) {
-			pointList.add(p);
-		}
-		reloadSpline();
-	}
+    /** Generic multiplier for Hermite splines which is needed to find the solution. */
+    private static final SimpleMatrix multBase = new SimpleMatrix(new double[][] {{1, 0, 0, 0},
+      {1, 1, 1, 1}, {0, 1, 0, 0}, {0, 1, 2, 3}}).invert();
+
+    private boolean safeReturn = false;
+    /**
+     * A HermiteSpline instance will take points and handle finding which function to use to find
+     * the solution points are formatted in.
+     *
+     * @param points Waypoint[] list of Waypoints to build the spline off of
+     */
+    public HermiteWaypointSpline(Waypoint[] points) {
+        for (Waypoint p : points) {
+            pointList.add(p);
+        }
+        reloadSpline();
+    }
 
     /**
-     * Takes in t and returns corresponding point from the piecewise spline function. The spline parts
-     * are parametric so [0 to 1] returns f( [0 to 1] ) from the first function. The second and so on
-     * splines will take in [n - 1 to n] and have it scaled for the corresponding function so that the
-     * return is equal to fn( [0 to 1] ).
-	 * @param t - value which determines x and y. And which piecewise spline function to use.
-	 * @return double[] containing {x, y}
-	 */
-	public double[] getPointAtT(double t) {
-		if (t < 0 || t > splineSections.size())
-			if (safeReturn) 
-				t = t < 0 ? 0 : splineSections.size();
-			else 
-				return null;
+     * Takes in t and returns corresponding point from the piecewise spline function. The spline
+     * parts are parametric so [0 to 1] returns f( [0 to 1] ) from the first function. The second
+     * and so on splines will take in [n - 1 to n] and have it scaled for the corresponding
+     * function so that the return is equal to fn( [0 to 1] ).
+     *
+     * @param t value which determines x and y, and which piecewise spline function to use
+     * @return double[] containing {x, y}
+     */
+    public double[] getPointAtT(double t) {
+        if (t < 0 || t > splineSections.size())
+            if (safeReturn) {
+                t = t < 0 ? 0 : splineSections.size();
+            }
+            else 
+                return null;
 
-		int index = (int) t;
-		t -= (int) t;
-		SimpleMatrix solution = new SimpleMatrix(new double[][] {{ 1, t, t * t, t * t * t }})
+        int index = (int) t;
+        t -= (int) t;
+        SimpleMatrix solution = new SimpleMatrix(new double[][] {{ 1, t, t * t, t * t * t }})
                 .mult(splineSections.get(index));
-		return new double[] { solution.get(0, 0), solution.get(0, 1)};
-	}
+        return new double[] { solution.get(0, 0), solution.get(0, 1)};
+    }
 
-	/**
+    /**
      * Takes in t and returns corresponding derivative from the piecewise spline function.
      * The spline parts are parametric so [0 to 1] returns f( [0 to 1] ) from the first function.
-     * The second and so on splines will take in [n - 1 to n] and have it scaled for the corresponding
-     * function so that the return is equal to fn( [0 to 1] ).
-	 * 
-	 * @param t value which determines x and y, and which piecewise spline function to use
-	 * @return double equal to dy/dx
-	 */
+     * The second and so on splines will take in [n - 1 to n] and have it scaled for the
+     * corresponding function so that the return is equal to fn( [0 to 1] ).
+     * 
+     * @param t value which determines x and y, and which piecewise spline function to use
+     * @return double equal to dy/dx
+     */
 
-	public double getDerivativeAtT(double t) {
-		if (t < 0 || t > splineSections.size())
-			if (safeReturn)
-				t = t < 0 ? 0 : splineSections.size();
-			else return Double.NaN;
-		int index = (int) t;
-		t -= ((int)t);
-		SimpleMatrix solution = new SimpleMatrix( new double[][] {{ 0, 1, 2 * t, 3 * t * t }} )
-        .mult( splineSections.get( index ) );
-		return solution.get(0, 1) / solution.get(0, 1);
-	}
+    public double getDerivativeAtT(double t) {
+        if (t < 0 || t > splineSections.size())
+            if (safeReturn) {
+                t = t < 0 ? 0 : splineSections.size();
+            }
+            else return Double.NaN;
+        int index = (int) t;
+        t -= (int) t;
+        SimpleMatrix solution = new SimpleMatrix(new double[][] {{ 0, 1, 2 * t, 3 * t * t }})
+                .mult(splineSections.get(index));
+        return solution.get(0, 1) / solution.get(0, 1);
+    }
 
-	/**
-	 * Reloads the spline segments based on the current contained Waypoints. Allows the option of
+    /**
+     * Reloads the spline segments based on the current contained Waypoints. Allows the option of
      * editing Waypoints in other classes then reloading the spline instead of needing to create a
      * new HermiteWaypointSpline instance.
-	 */
-	public void reloadSpline() {
-		splineSections.clear();
-		if (pointList.size() < 2) return;
+     */
+    public void reloadSpline() {
+        splineSections.clear();
+        if (pointList.size() < 2) return;
 
-		//check if last point has derivative since the gaps are filled in based on the following points
-		if (pointList.get(pointList.size() - 1).autoAssignDerivative) {
-			pointList.get(pointList.size() - 1).setDerivative(pointList.get(pointList.size() - 1).x
-          - pointList.get(pointList.size() - 2 ).x,pointList.get(pointList.size() - 1).y
+        //check if last point has derivative since the gaps are filled in based on the following points
+        if (pointList.get(pointList.size() - 1).autoAssignDerivative) {
+            pointList.get(pointList.size() - 1).setDerivative(pointList.get(pointList.size() - 1).x
+          - pointList.get(pointList.size() - 2).x,pointList.get(pointList.size() - 1).y
           - pointList.get(pointList.size() - 2).y);
-			pointList.get(pointList.size() - 1).autoAssignDerivative = true;
-		}
+            pointList.get(pointList.size() - 1).autoAssignDerivative = true;
+        }
 
-		for (int i = 0; i < pointList.size() - 1; i++) {
-			if (pointList.get(i).autoAssignDerivative && i != 0) {
-				pointList.get(i).setDerivative((pointList.get(i + 1).x - pointList.get(i - 1).x),
-            (pointList.get(i + 1).y - pointList.get(i - 1).y));
-				pointList.get(i).autoAssignDerivative = true;
-			}
-			loadSplineBetweenWaypoints(pointList.get(i), pointList.get(i + 1));
-		}
-	}
-
-	/**
-	 * Domain of t is [0, getSplineDomain()]
-	 * @return the maximum input value of t 
-	 */
-	public double getSplineDomain() {
-		return splineSections.size();
-	}
-
-	/**
-	 * Returns the matrices used to generate the spline.
-	 * @return SimpleMatrix[] array of coefficients
-	 */
-	public SimpleMatrix[] getSplineSections() { 
-		return (SimpleMatrix[]) splineSections.toArray(); 
-	}
-	/**
-	 * If t is out of the range of the spline, safe return will return the last point on the spline
-     * otherwise null is returned.
-	 * @param arg new value for safe return
-	 */
-	public void setSafeReturn(boolean arg) {
-		safeReturn = arg;
-	}
-
-	/**
-	 * Appends an array of points to the point list then reloads the spline sections.
-	 * @param points Array of Waypoints to add
-	 */
-	public void appendPoints(Waypoint[] points) {
-		for (Waypoint w : points) {
-      pointList.add(w);
+        for (int i = 0; i < pointList.size() - 1; i++) {
+            if (pointList.get(i).autoAssignDerivative && i != 0) {
+                pointList.get(i).setDerivative((pointList.get(i + 1).x - pointList.get(i - 1).x),
+                        (pointList.get(i + 1).y - pointList.get(i - 1).y));
+                pointList.get(i).autoAssignDerivative = true;
+            }
+            loadSplineBetweenWaypoints(pointList.get(i), pointList.get(i + 1));
+        }
     }
-		reloadSpline();
-	}
 
-	/**
-	 * Appends a point, then updates the spline.
-	 * @param point the Waypoint to be added
-	 */
-	public void appendPoint(Waypoint point) {
-		if (pointList.size() == 0) {
-			pointList.add(point);
-			return;
-		}
-		pointList.add(point);
-		reloadSpline();
-	}
+    /**
+     * Domain of t is [0, getSplineDomain()].
+     *
+     * @return the maximum input value of t 
+     */
+    public double getSplineDomain() {
+        return splineSections.size();
+    }
 
-	/**
-	 * Returns an ArrayList of Waypoints that can be edited then be applied to the spline using
+    /**
+     * Returns the matrices used to generate the spline.
+     *
+     * @return SimpleMatrix[] array of coefficients
+     */
+    public SimpleMatrix[] getSplineSections() { 
+        return (SimpleMatrix[]) splineSections.toArray(); 
+    }
+    /**
+     * If t is out of the range of the spline, safe return will return the last point on the spline
+     * otherwise null is returned.
+     *
+     * @param arg new value for safe return
+     */
+    public void setSafeReturn(boolean arg) {
+        safeReturn = arg;
+    }
+
+    /**
+     * Appends an array of points to the point list then reloads the spline sections.
+     *
+     * @param points Array of Waypoints to add
+     */
+    public void appendPoints(Waypoint[] points) {
+        for (Waypoint w : points) {
+            pointList.add(w);
+    }
+        reloadSpline();
+    }
+
+    /**
+     * Appends a point, then updates the spline.
+     *
+     * @param point the Waypoint to be added
+     */
+    public void appendPoint(Waypoint point) {
+        if (pointList.size() == 0) {
+            pointList.add(point);
+            return;
+        }
+        pointList.add(point);
+        reloadSpline();
+    }
+
+    /**
+     * Returns an ArrayList of Waypoints that can be edited then be applied to the spline using
      * reloadSpline(). Note that if the points some of the points on this list were not added with
      * appendPoint() or appendPoints(), the spline may not have been updated to include them.
-	 * 
-	 * @return a list of the current points
-	 */
-	public List<Waypoint> getWaypointList() {
-		return pointList;
-	}
+     *
+     * @return a list of the current points
+     */
+    public List<Waypoint> getWaypointList() {
+        return pointList;
+    }
 
-	
-	private void loadSplineBetweenWaypoints(Waypoint a, Waypoint b) {
-		double mult = Math.sqrt(Math.pow(a.x - b.x, 2)+Math.pow(a.y - b.y, 2));
-		splineSections.add( generateSplineSection(a.x, a.y, b.x, b.y, a.derivativeX * mult,
+    
+    private void loadSplineBetweenWaypoints(Waypoint a, Waypoint b) {
+        double mult = Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+        splineSections.add(generateSplineSection(a.x, a.y, b.x, b.y, a.derivativeX * mult,
         a.derivativeY * mult, b.derivativeX * mult, b.derivativeY * mult));
-	}
+    }
 
-	/**
+    /**
      * Generates a Hermite spline section based on the given end points and derivatives.
-	 * @param ax x of point A
-	 * @param ay y of point A
-	 * @param bx x of point B
-	 * @param by y of point B
-	 * @param dax x derivative of the spline at point A
-	 * @param day y der/vative of the spline at point A
-	 * @param dbx x derivative of the spline at point B
-	 * @param dby y derivative of the spline at point B
-	 * @return SimpleMatrix of spline coefficients
-	 */
-	public static SimpleMatrix generateSplineSection(double ax, double ay, double bx, double by,
+     *
+     * @param ax x of point A
+     * @param ay y of point A
+     * @param bx x of point B
+     * @param by y of point B
+     * @param dax x derivative of the spline at point A
+     * @param day y der/vative of the spline at point A
+     * @param dbx x derivative of the spline at point B
+     * @param dby y derivative of the spline at point B
+     * @return SimpleMatrix of spline coefficients
+     */
+    public static SimpleMatrix generateSplineSection(double ax, double ay, double bx, double by,
       double dax, double day, double dbx, double dby) {
-		return  multBase.mult(new SimpleMatrix(new double[][] {{ax, ay}, {bx, by}, {dax, day},
+        return multBase.mult(new SimpleMatrix(new double[][] {{ax, ay}, {bx, by}, {dax, day},
         {dbx, dby}}));
-	}
+    }
 }
--- src/main/java/org/team114/lib/pathgenerator/Waypoint.java
+++ src/main/java/org/team114/lib/pathgenerator/Waypoint.java
@@ -2,99 +2,106 @@ package org.team114.lib.pathgenerator;
 
 public class Waypoint {
 
-	public double x;
+    public double x;
     public double y;
     public double derivativeX;
     public double derivativeY;
 
-	public boolean autoAssignDerivative = true;
+    public boolean autoAssignDerivative = true;
 
-	/**
-	 * Creates a new Waypoint at (x, y) with an unspecified derivative.
-	 * @param x X coordinate of the Waypoint
-	 * @param y Y coordinate of the Waypoint
-	 */
-	public Waypoint(double x, double y) {
-		this.x = x;
-		this.y = y;
-	}
-	
-	/**
-	 * Creates a new Waypoint at (x, y) then sets the derivative using the specified components with
-	 * setAngle(double angle).
-	 * Note that the angle is in radians.
-	 * @param x X coordinate of the Waypoint
-	 * @param y Y coordinate of the Waypoint
-	 * @param angle angle of the derivative of the spline that goes through this point
-	 */
-	public Waypoint(double x, double y, double angle) {
-		this(x, y);
-		setAngle(angle);
-	}
+    /**
+     * Creates a new Waypoint at (x, y) with an unspecified derivative.
+     *
+     * @param x X coordinate of the Waypoint
+     * @param y Y coordinate of the Waypoint
+     */
+    public Waypoint(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    /**
+     * Creates a new Waypoint at (x, y) then sets the derivative using the specified components with
+     * setAngle(double angle). Note that the angle is in radians.
+     *
+     * @param x X coordinate of the Waypoint
+     * @param y Y coordinate of the Waypoint
+     * @param angle angle of the derivative of the spline that goes through this point
+     */
+    public Waypoint(double x, double y, double angle) {
+        this(x, y);
+        setAngle(angle);
+    }
 
-	/**
-	 * Creates a new Waypoint at (x, y) then sets the derivative using the specified components with
+    /**
+     * Creates a new Waypoint at (x, y) then sets the derivative using the specified components with
      * setDerivative(double x, double y).
-	 * @param x X coordinate of the Waypoint
-	 * @param y Y coordinate of the Waypoint
-	 * @param xDerivative the X component of the derivative of the spline that goes through this point
-	 * @param yDerivative the Y component of the derivative of the spline that goes through this point
-	 */
-	public Waypoint(double x, double y, double xDerivative, double yDerivative) {
-		this(x, y);
-		setDerivative(xDerivative, yDerivative);
-	}
+     *
+     * @param x X coordinate of the Waypoint
+     * @param y Y coordinate of the Waypoint
+     * @param xDerivative the X component of the derivative of the spline that goes through this point
+     * @param yDerivative the Y component of the derivative of the spline that goes through this point
+     */
+    public Waypoint(double x, double y, double xDerivative, double yDerivative) {
+        this(x, y);
+        setDerivative(xDerivative, yDerivative);
+    }
 
-	/**
-	 * Takes in a ratio of x and y and scales them to maintain y/x while putting x and y in a usable range.
-	 * (Prevents unnecessary loops in final splines)
-	 * @param x derivative x component
-	 * @param y derivative y component
-	 * @return the Waypoint that was assigned the derivative to allow for shorter code in application. A new Waypoint
-     * is not created by reassigning the derivative.
-	 */
-	public Waypoint setDerivative(double x, double y) {
-		derivativeY = y / Math.sqrt(x * x + y * y);
-		derivativeX = x / Math.sqrt(x * x + y * y);
-		autoAssignDerivative = true;
-		return this;
-	}
+    /**
+     * Takes in a ratio of x and y and scales them to maintain y/x while putting x and y in a
+     * usable range, preventing unnecessary loops in final splines.g
+     *
+     * @param x derivative x component
+     * @param y derivative y component
+     * @return the Waypoint that was assigned the derivative to allow for method chaining
+     * in the application. A new Waypoint is not created by reassigning the derivative.
+     */
+    public Waypoint setDerivative(double x, double y) {
+        derivativeY = y / Math.sqrt(x * x + y * y);
+        derivativeX = x / Math.sqrt(x * x + y * y);
+        autoAssignDerivative = true;
+        return this;
+    }
 
-	/**
-	 * Takes in a ratio of x and y for the derivative, but does not scale them. If values are too large or small,
-     * the curve of the spline may not be visable or unnessesary loops may be created in the spline.
-	 * @param x derivative x component
-	 * @param y derivative y component
-	 * @return the Waypoint that was assigned the derivative to allow for shorter code in application. A new Waypoint
-     * is not created by reassigning the derivative.
-	 */
-	public Waypoint setDerivativeUnscaled(double x, double y) {
-		derivativeX = x;
-		derivativeY = y;
-		autoAssignDerivative = false;
-		return this;
-	}
+    /**
+     * Takes in a ratio of x and y for the derivative, but does not scale them. If values are too
+     * large or small, the curve of the spline may not be visible or unnecessary loops may be
+     * created in the spline.
+     *
+     * @param x derivative x component
+     * @param y derivative y component
+     * @return the Waypoint that was assigned the derivative to allow for method chaining in the
+     * application. A new Waypoint is not created by reassigning the derivative.
+     */
+    public Waypoint setDerivativeUnscaled(double x, double y) {
+        derivativeX = x;
+        derivativeY = y;
+        autoAssignDerivative = false;
+        return this;
+    }
 
-	/**
-	 * Takes in an angle and uses it to determine the derivative x and y components.
-	 * @param angle the angle of the derivative in radians
-	 * @return the Waypoint that was assigned the derivative to allow for shorter code in application. A new Waypoint
-     * is not created by reassigning the derivative.
-	 */
-	public Waypoint setAngle(double angle) {
-		derivativeY = Math.sin(angle);
-		derivativeX = Math.cos(angle);
-		autoAssignDerivative = false;
-		return this;
-	}
+    /**
+     * Takes in an angle and uses it to determine the derivative x and y components.
+     *
+     * @param angle the angle of the derivative in radians
+     * @return the Waypoint that was assigned the derivative to allow for method chaining
+     * in the application. A new Waypoint is not created by reassigning the derivative.
+     */
+    public Waypoint setAngle(double angle) {
+        derivativeY = Math.sin(angle);
+        derivativeX = Math.cos(angle);
+        autoAssignDerivative = false;
+        return this;
+    }
 
-	/**
-	 * Returns the current derivative of a waypoint in the form of the y/x. If a Waypoint has beem auto-assigned a
-     * derivative, that will be returned.
-	 * @return the derivative at a Waypoint
-	 */
-	public double getDerivative() {
-		return derivativeY / derivativeX;
-	}
-	
+    /**
+     * Returns the current derivative of a waypoint in the form of the y/x. If a Waypoint has
+     * been auto-assigned a derivative, that will be returned.
+     *
+     * @return the derivative at a Waypoint
+     */
+    public double getDerivative() {
+        return derivativeY / derivativeX;
+    }
+    
 }
