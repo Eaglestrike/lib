8e874c8eece6140b8b1c21f679bd95fe05c299ed
Josh Hejna <josh.hejna@gmail.com>
2017-12-08 17:36:52 -0800

    Add tests for Polynomial

---
+++ src/test/java/org/team114/lib/pathgenerator/PolynomialTest.java
@@ -0,0 +1,29 @@
+package org.team114.lib.pathgenerator;
+
+
+import org.junit.*;
+import org.team114.lib.util.Epsilon;
+
+// NOTE: this test depends on EpsilonTest! If someone knows how to tell JUnit that, let me know.
+public class PolynomialTest {
+
+    @Test
+    public void lowCoefficientPolynomialsEvaluation() {
+        Polynomial p = new Polynomial(new double[]{7, 3, 5, 15, 2.2, -5.5, 0.0145, -0.234});
+        Assert.assertEquals(35312.3125,p.eval(-5), 1.0);
+        Assert.assertEquals(-757174244.355,p.eval(22.7), 1.0);
+        Assert.assertEquals(7.03987801579,p.eval(0.013), 0.1);
+        Assert.assertEquals(4.87014855263,p.eval(-0.547), 0.1);
+    }
+
+    @Test
+    public void lowCoefficientPolynomialsDerivatives() {
+        Polynomial p = new Polynomial(new double[]{7, 3, 5, 15, 2.2, -5.5, 0.0145, -0.234}).ddx();
+        System.out.println(p.coefficients[1]);
+        Assert.assertEquals(-43075.125, p.eval(-5), 1.0);
+        Assert.assertEquals(-230765196.422,p.eval(22.7),1.0);
+        p = p.ddx();
+        Assert.assertEquals(30.7530816704,p.eval(0.23), 0.5);
+        Assert.assertEquals(53407.243561,p.eval(-5.2), 1.0);
+    }
+}
