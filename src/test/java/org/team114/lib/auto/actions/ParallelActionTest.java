eba987b433d5dc70a938ebc3cb59fe5cc2842301
Aris (Karim) Merchant <wizkarim@gmail.com>
2017-12-10 00:05:57 -0800

    Latch based ParallelAction test
    Previously, ParallelAction was tested with a timeout, but this
    implementation was messy, time consuming, prone to random failure. A
    CountDownLatch is now used instead, which causes the code to wait
    until all actions have been run. A timeout is now used only to check
    if the test halts indefinitely.

--- src/test/java/org/team114/lib/auto/actions/ParallelActionTest.java
+++ src/test/java/org/team114/lib/auto/actions/ParallelActionTest.java
@@ -5,13 +5,17 @@ import org.mockito.Mockito;
 
 import org.team114.lib.auto.Action;
 
+import java.util.concurrent.CountDownLatch;
+
 import static org.mockito.Mockito.times;
 
 public class ParallelActionTest {
 
+    private CountDownLatch latch = new CountDownLatch(3);
     private Runnable runnable = ()  -> {
+        latch.countDown();
         try {
-            Thread.sleep(25);
+            latch.await();
         } catch (InterruptedException e) {
             System.out.print("Test interrupted: " + e.getMessage());
         }
@@ -20,9 +24,10 @@ public class ParallelActionTest {
     private Action spy = Mockito.spy(new RunnerAction(runnable));
     private Action parallel = new ParallelAction(spy, spy, spy);
 
-    @Test(timeout = 60)
+    @Test(timeout = 250)
     public void testConcurrentExecution() {
         parallel.run();
+
         Mockito.verify(spy, times(3)).run();
         Mockito.verifyNoMoreInteractions(spy);
     }
