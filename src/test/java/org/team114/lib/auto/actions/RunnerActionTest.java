e491f28d46ddd7143e4afcd6193aeeb896690336
Aris (Karim) Merchant <wizkarim@gmail.com>
2017-12-10 13:00:01 -0800

    Fix RunnerActionTest
    RunnerActionTest did not actually test RunnerAction, instead directly
    testing Runnable.

--- src/test/java/org/team114/lib/auto/actions/RunnerActionTest.java
+++ src/test/java/org/team114/lib/auto/actions/RunnerActionTest.java
@@ -2,16 +2,18 @@ package org.team114.lib.auto.actions;
 
 import org.junit.*;
 import org.mockito.*;
+import org.team114.lib.auto.Action;
 
 import static org.mockito.Mockito.times;
 
 public class RunnerActionTest {
 
     private Runnable runnable = Mockito.mock(Runnable.class);
+    private Action action = new RunnerAction(runnable);
 
     @Test
     public void testRun() {
-        runnable.run();
+        action.run();
         Mockito.verify(runnable, times(1)).run();
     }
 
